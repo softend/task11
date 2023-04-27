a394dbaad49907404e61ec1a04b4f1f5066d4563
Ilya Zenchenko <ilya.zenchenko@icloud.com>
2023-04-27 12:17:16 +0300

    Initial commit

---
+++ src/main/java/ru/zenchenko/task11/SpringBootFirstTaskApplication.java
@@ -0,0 +1,13 @@
+package ru.zenchenko.task11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstTaskApplication {
+// Запуск Приложения
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstTaskApplication.class, args);
+	}
+
+}
